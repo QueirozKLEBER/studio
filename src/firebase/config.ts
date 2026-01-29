9ed9932e4ba4f20992a452a0085be1be4be85e92
Kleber Queiroz <contatokleber19@gmail.com>
2026-01-29 12:27:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1744255692-17fbe",
+  "appId": "1:10276619923:web:c92a50dcb4195387ee2f66",
+  "apiKey": "AIzaSyByNIvVF3BTN1989lsJImR70C1kyeDwfAE",
+  "authDomain": "studio-1744255692-17fbe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "10276619923"
+};
